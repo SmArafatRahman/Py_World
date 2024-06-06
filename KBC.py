344c98ee7f5522f4f5a6040a3c5e0d01c82d9412
S M Arafat Rahman <97359833+SmArafatRahman@users.noreply.github.com>
2024-06-07 04:45:45 +0600

    KBC.py

---
+++ KBC.py
@@ -0,0 +1,188 @@
+#Kon Banaga Kororpati
+def question(num):
+  print("Your Que is on your display.")
+  print("Which team won the FIFA World cup 2022?")
+  print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+  ans = int(input())
+  if(ans==1):
+    num+=1
+    print("Congratulations!!!!!!\nCorrect Answer.")
+    print("Your Next Question is on your display.")
+    print("Which team won the FIFA World cup 2022?")
+    print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+    ans = int(input())
+    if(ans==1):
+      num+=1
+      print("Congratulations!!!!!!\nCorrect Answer.")
+      print("Your Next Question is on your display.")
+      print("Which team won the FIFA World cup 2022?")
+      print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+      ans = int(input())
+      if(ans==1):
+        num+=1
+        print("Congratulations!!!!!!\nCorrect Answer.")
+        print("Your Next Question is on your display.")
+        print("Which team won the FIFA World cup 2022?")
+        print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+        ans = int(input())
+        if(ans==1):
+          num+=1
+          print("Congratulations!!!!!!\nCorrect Answer.")
+          print("Your Next Question is on your display.")
+          print("Which team won the FIFA World cup 2022?")
+          print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+          ans = int(input())
+          if(ans==1):
+            num+=1
+            print("Congratulations!!!!!!\nCorrect Answer.")
+            print("Your Next Question is on your display.")
+            print("Which team won the FIFA World cup 2022?")
+            print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+            ans = int(input())
+            if(ans==1):
+              num+=1
+              print("Congratulations!!!!!!\nCorrect Answer.")
+              print("Your Next Question is on your display.")
+              print("Which team won the FIFA World cup 2022?")
+              print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+              ans = int(input())
+              if(ans==1):
+                num+=1
+                print("Congratulations!!!!!!\nCorrect Answer.")
+                print("Your Next Question is on your display.")
+                print("Which team won the FIFA World cup 2022?")
+                print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+                ans = int(input())
+                if(ans==1):
+                  num+=1
+                  print("Congratulations!!!!!!\nCorrect Answer.")
+                  print("Your Next Question is on your display.")
+                  print("Which team won the FIFA World cup 2022?")
+                  print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+                  ans = int(input())
+                  if(ans==1):
+                    num+=1
+                    print("Congratulations!!!!!!\nCorrect Answer.")
+                    print("Your Next Question is on your display.")
+                    print("Which team won the FIFA World cup 2022?")
+                    print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+                    ans = int(input())
+                    if(ans==1):
+                      num+=1
+                      print("Congratulations!!!!!!\nCorrect Answer.")
+                      print("Your Next Question is on your display.")
+                      print("Which team won the FIFA World cup 2022?")
+                      print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+                      ans = int(input())
+                      if(ans==1):
+                        num+=1
+                        print("Congratulations!!!!!!\nCorrect Answer.")
+                        print("Your Next Question is on your display.")
+                        print("Which team won the FIFA World cup 2022?")
+                        print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+                        ans = int(input())
+                        if(ans==1):
+                          num+=1
+                          print("Congratulations!!!!!!\nCorrect Answer.")
+                          print("Your Next Question is on your display.")
+                          print("Which team won the FIFA World cup 2022?")
+                          print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+                          ans = int(input())
+                          if(ans==1):
+                            num+=1
+                            print("Congratulations!!!!!!\nCorrect Answer.")
+                            print("Your Next Question is on your display.")
+                            print("Which team won the FIFA World cup 2022?")
+                            print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+                            ans = int(input())
+                            if(ans==1):
+                              num+=1
+                              print("Congratulations!!!!!!\nCorrect Answer.")
+                              print("Your Next Question is on your display.")
+                              print("Which team won the FIFA World cup 2022?")
+                              print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+                              ans = int(input())
+                              if(ans==1):
+                                num+=1
+                                print("Congratulations!!!!!!\nCorrect Answer.")
+                                print("Your Next Question is on your display.")
+                                print("Which team won the FIFA World cup 2022?")
+                                print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+                                ans = int(input())
+                                if(ans==1):
+                                  num+=1
+                                  print("Congratulations!!!!!!\nCorrect Answer.")
+                                  print("Your Next Question is on your display.")
+                                  print("Which team won the FIFA World cup 2022?")
+                                  print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+                                  ans = int(input())
+                                  if(ans==1):
+                                    num+=1
+                                    print("Congratulations!!!!!!\nCorrect Answer.")
+                                    print("Your Next Question is on your display.")
+                                    print("Which team won the FIFA World cup 2022?")
+                                    print("1.Argentina\n2.Brazil\n3.Germany\n4.France")
+                                    ans = int(input())
+                                    print("!!!!!!!!You Won 7Cr!!!!!!!!")
+                                    return num
+                                  else:
+                                    print("Wrong Answer")
+                                    return num
+                                else:
+                                  print("Wrong Answer")
+                                  return num
+                              else:
+                                print("Wrong Answer")
+                                return num
+                            else:
+                              print("Wrong Answer")
+                              return num
+                          else:
+                            print("Wrong Answer")
+                            return num
+                        else:
+                          print("Wrong Answer")
+                          return num
+                      else:
+                        print("Wrong Answer")
+                        return num
+                    else:
+                      print("Wrong Answer")
+                      return num
+                  else:
+                    print("Wrong Answer")
+                    return num
+                else:
+                  print("Wrong Answer")
+                  return num
+              else:
+                print("Wrong Answer")
+                return num
+            else:
+              print("Wrong Answer")
+              return num
+          else:
+            print("Wrong Answer")
+            return num
+        else:
+          print("Wrong Answer")
+          return num
+      else:
+        print("Wrong Answer")
+        return num
+    else:
+      print("Wrong Answer")
+      return num
+  else:
+    print("Wrong Answer")
+    return num
+
+print("Welcome to KBC")
+que = question(1)
+reward= que
+if(reward==1):
+  print("You Won Nothing.\nThank you for your participation.\nBetter luck next time")
+else:
+  print("Congratulations!!!!!!")
+  PrizeList = (1000, 2000, 3000, 5000, 10000, 20000, 40000, 80000, 160000, 320000, 640000, 1250000, 2500000, 5000000, 10000000, 70000000)
+  print('You Won',PrizeList[que-2])
